c0439307f12097041120d9df768fb4a5eb0cede8
HumaCode <humaidizakaria1998@gmail.com>
2026-05-31 10:50:22 +0700

    fix: resolve controller trait inheritance issue in Laravel 11+

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Traits\HasPermission;
+use App\Traits\HasMenuPermission;
+
 abstract class Controller
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, HasPermission, HasMenuPermission; 
 }
--- app/Traits/HasMenuPermission.php
+++ app/Traits/HasMenuPermission.php
@@ -4,7 +4,7 @@
 namespace App\Traits;
 
 use App\Models\Konfigurasi\Menu;
-use App\Models\Shield\Permission;
+use App\Models\Permission;
 
 trait HasMenuPermission
 {
